cd5cc6fb31c8975fd5a8a7f42ce66266731b174b
Ivan Pudin <ivanpudin9@gmail.com>
2026-03-21 16:52:57 +0200

    init

---
+++ bidi_schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS bidi_db;
+CREATE DATABASE bidi_db;
+USE bidi_db;
+
+CREATE TABLE Project (
+
+);
+
+CREATE TABLE Customer (
+
+);
+
+CREATE TABLE Location (
+
+);
+
+CREATE TABLE Department (
+
+);
+
+CREATE TABLE Employee (
+
+);
+
+CREATE TABLE Role (
+
+);
+
+CREATE TABLE UserGroup (
+    
+);
